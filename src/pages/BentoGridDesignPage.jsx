887f7e04b55f1d94002ab26ec1a7cbcbc965b947
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-01-25 01:18:45 -0500

    Client31

--- src/pages/BentoGridDesignPage.jsx
+++ src/pages/BentoGridDesignPage.jsx
@@ -10,6 +10,16 @@ import tapePicture from "../images/tape_picture(800x800).png";
 import tapePicture2 from "../images/tape_picture2(800x800).png";
 import g1jo from "../gif/G1JO.gif";
 
+import picturePeices1 from "../images/picture_peices/picture_peices1.png";
+import picturePeices2 from "../images/picture_peices/picture_peices2.png";
+import picturePeices3 from "../images/picture_peices/picture_peices3.png";
+import picturePeices4 from "../images/picture_peices/picture_peices4.png";
+import picturePeices5 from "../images/picture_peices/picture_peices5.png";
+import picturePeices6 from "../images/picture_peices/picture_peices6.png";
+import picturePeices7 from "../images/picture_peices/picture_peices7.png";
+import picturePeices8 from "../images/picture_peices/picture_peices8.png";
+import picturePeices9 from "../images/picture_peices/picture_peices9.png";
+
 import MovieAddict from "../images/screenshots/Movie-Addict(1900x1080).png";
 import Tesla from "../images/screenshots/Tesla(1900x1080).png";
 import NutriKcal from "../images/screenshots/Nutri-Kcal(1900x1080).png";
@@ -369,9 +379,9 @@ function BentoGridDesignPage() {
               <div className="Grid-2-1 fade"></div>
               <div className="Grid-2-2 fade"></div>
               <div className="Grid-2-3 fade"></div>
-              <div className="Grid-2-4 fade"></div>
-              <div className="Grid-2-5 fade"></div>
-              <div className="Grid-2-6 fade"></div>
+              <div className="Grid-2-4 fade"><img src={picturePeices1}  /></div>
+              <div className="Grid-2-5 fade"><img src={picturePeices2}  /></div>
+              <div className="Grid-2-6 fade"><img src={picturePeices3}  /></div>
               <div className="Grid-2-7 fade"></div>
               <div className="Grid-2-8 fade"></div>
               <div className="Grid-2-9 fade"></div>
@@ -383,8 +393,8 @@ function BentoGridDesignPage() {
               <div className="Grid-3-2 fade"></div>
               <div className="Grid-3-3 fade"></div>
               <div className="Grid-3-4 fade"></div>
-              <div className="Grid-3-5 fade"></div>
-              <div className="Grid-3-6 fade"></div>
+              <div className="Grid-3-5 fade"><img src={picturePeices5}  /></div>
+              <div className="Grid-3-6 fade"><img src={picturePeices6}  /></div>
               <div className="Grid-3-7 fade"></div>
               <div className="Grid-3-8 fade"></div>
               <div className="Grid-3-9 fade"></div>
@@ -397,7 +407,7 @@ function BentoGridDesignPage() {
               <div className="Grid-4-3 fade"></div>
               <div className="Grid-4-4 fade"></div>
               <div className="Grid-4-5 fade"></div>
-              <div className="Grid-4-6 fade"></div>
+              <div className="Grid-4-6 fade"><img src={picturePeices9}  /></div>
               <div className="Grid-4-7 fade"></div>
               <div className="Grid-4-8 fade"></div>
               <div className="Grid-4-9 fade"></div>
@@ -568,6 +578,14 @@ function BentoGridDesignPage() {
               <div className="project-img-box">
                   <img src={myPicture2}  />
               </div>
+              {
+                /* ______________________________________________________________________________________________________________________________________________________ */
+              }
+              {/*
+              <div className="r2-c4-location">
+                  <img src={picturePeices1}  />
+              </div>
+              */}
               {
                 /* ______________________________________________________________________________________________________________________________________________________ */
               }
