69271ca02890381701330ad52c3fb0ab113315b1
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-08-28 01:07:50 -0400

    Removed unused image imports from page component

--- src/pages/BentoGridDesignPage.jsx
+++ src/pages/BentoGridDesignPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
-import { formatDateWithOrdinal } from "../helpers/FormatDate.jsx";
-//
+
 import "../styles/BentoGridDesignCSS.css";
 import '../index.css';
 
@@ -16,13 +15,6 @@ import DisplayMyPicture from '../components/projects/DisplayMyPicture.jsx';
 import DisplayProject from '../components/projects/DisplayProject.jsx';
 import DisplayProjectDetail from '../components/projects/DisplayProjectDetail.jsx';
 
-import jaturaputLogo from '../images/logos/jaturaput-logo.png';
-import myPicture1 from "../images/my_picture1(500x750).png";
-import myPicture2 from "../images/my_picture2(800x800).png";
-import tapePicture from "../images/tape_picture(800x800).png";
-import tapePicture2 from "../images/tape_picture2(800x800).png";
-import g1jo from "../gif/G1JO.gif";
-
 import projects from '../data/JaturaputProject.js';
 
 function BentoGridDesignPage() {
@@ -31,8 +23,6 @@ function BentoGridDesignPage() {
   const [filteredProjects, setFilteredProjects] = useState(projects.filter((project) => project.type === "WEB & MOBILE DESIGN"));
   const [selectedProject, setSelectedProject] = useState(filteredProjects[0]);
   
-  const [currentIndex, setCurrentIndex] = useState(0);
-
   const [showDetails, setShowDetails] = useState(false);
 
   useEffect(() => {
@@ -69,12 +59,6 @@ function BentoGridDesignPage() {
     setSelectedProject(filteredProjects[newIndex]);
   };
 
-  /*
-  const formatDetail = (text) => {
-    return text.replace(/&nbsp;/g, " ").trim();
-  };
-  */
-
   const calculateDuration = (start, end) => {
     const startDate = new Date(start);
     const endDate = new Date(end);
@@ -83,20 +67,6 @@ function BentoGridDesignPage() {
     return daysDifference;
   };
   
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return new Intl.DateTimeFormat("en-US", { month: "short", day: "numeric", year: "numeric" }).format(date);
-  };
-
-  const duration = calculateDuration(
-    projects[0].project_duration.start,
-    projects[0].project_duration.end
-  );
-
-  /*
-  console.log(`Project Duration: ${duration} days`);
-  */
-
   return (
     <>
       {
@@ -108,14 +78,8 @@ function BentoGridDesignPage() {
       {
         /* >Live Server:Open with Live Server */
       }
-      {
-        /* ______________________________________________________________________________________________________________________________________________________ */
-      }
       <div className="body-bento-grid-design">
         <div className="container">
-          {
-            /* ______________________________________________________________________________________________________________________________________________________ */
-          }
           <div className="scrolling-slides">
             <div className="Grid-container">
 
