90af61531ef1cb092230a2b046a19ac825b8037a
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2025-09-22 15:16:03 -0400

    update my second picture to solve the problem about displaying on the phone screen6

--- src/components/projects/DisplayMyPictureProfile2.jsx
+++ src/components/projects/DisplayMyPictureProfile2.jsx
@@ -1,11 +1,15 @@
-import myPicture1 from "../../images/my_picture1_500x750.png";
+import myPicture2 from "../../images/my_picture1_500x750.png";
+import myPicture2Slice from "../../images/my_picture2_800x800.png"; // import for CSS slice
 import "../../styles/layout/ResponsiveGrid.css";
 import "./DisplayMyPictureProfile2.css";
 
 function DisplayMyPictureProfile2() {
   return (
-    <div className="profile-img-box2">
-      <img src={myPicture1} alt="My Profile" />
+    <div
+      className="profile-img-box2"
+      style={{ "--profile-slice": `url(${myPicture2Slice})` }}  // pass slice as CSS var
+    >
+      <img src={myPicture2} alt="My Profile" />
     </div>
   );
 }
