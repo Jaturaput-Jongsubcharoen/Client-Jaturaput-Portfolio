7883eeb674ac91b0812924a721663ecbdb597ffe
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2026-07-04 16:06:59 -0400

    adding new resume pdf file

--- src/data/JaturaputResume.js
+++ src/data/JaturaputResume.js
@@ -5,7 +5,7 @@ const JaturaputResume = [
     id: "resume_ai",
     title: "Resume",
     name: "Résumé (AI Track)",                 
-    src: "/resume/Resume_Jaturaput_Jongsubcharoen_AI.pdf",
+    src: "/resume/Resume_Jaturaput(Mac)Jongsubcharoen_Data_Analyst.pdf",
   },
 ];
 
