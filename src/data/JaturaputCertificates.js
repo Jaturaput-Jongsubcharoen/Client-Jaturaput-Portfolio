411bae8320b0d34215dc35afd02dc48b923f373f
Jaturaput-Jongsubcharoen <macker.jong@gmail.com>
2026-07-06 14:16:09 -0400

    Updating all certificates

--- src/data/JaturaputCertificates.js
+++ src/data/JaturaputCertificates.js
@@ -1,9 +1,39 @@
 const JaturaputCertificates = [
   {
+    id: "centennial",
+    title: "Certificate",
+    name: "Centennial College Certificate – Jaturaput (Mac) Jongsubcharoen",
+    src: "/certificate/Centennial_College_Certificate_Jaturaput(Mac)Jongsubcharoen.pdf",
+  },
+  {
     id: "barch",
     title: "Certificate",
     name: "Bachelor of Architecture – Certificate",
     src: "/certificate/4_Jaturaput_Bachelor_of_Architecture_Certificate.pdf",
+  },
+  {
+    id: "fleetzero",
+    title: "Certificate",
+    name: "Jaturaput Jongsubcharoen – Certificate FleetZero Centennial Innovates Jam",
+    src: "/certificate/Jaturaput  Jongsubcharoen - Certificate FleetZero-Centennial Innovates Jam.pdf",
+  },
+  {
+    id: "motion-graphics",
+    title: "Certificate",
+    name: "Motion Graphics Designer – Riipen Certificate",
+    src: "/certificate/Motion-Graphics-Designer_Certifiacte_Riipen_ Jaturaput (Mac) Jongsubcharoen.pdf",
+  },
+  {
+    id: "website-builder",
+    title: "Certificate",
+    name: "Website Builder – Riipen Certificate",
+    src: "/certificate/Website-Builder_Certifiacte_Riipen_ Jaturaput (Mac) Jongsubcharoen.pdf",
+  },
+  {
+    id: "cybersecurity",
+    title: "Certificate",
+    name: "Foundations of Cybersecurity – Certificate",
+    src: "/certificate/Foundations-of-Cybersecurity_Certificate_Jaturaput_Jongsubcharoen.pdf",
   },
 ];
 
